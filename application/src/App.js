42c0e7255646ffe6c4d5c7609d06cbdc04be59e6
manos <manosvek@gmail.com>
2020-07-28 16:54:18 +0300

    Added 7-days forecast for second city

--- application/src/App.js
+++ application/src/App.js
@@ -7,6 +7,9 @@ import Tabs from './components/Tabs';
 
 const API_KEY = "9c8c814f6f94f1bb0ad930ceada1d790";
 
+var data3;
+var days = [1,2,3,4,5,6,7];
+
 class App extends React.Component {
   state = {
     temperature1: undefined,
@@ -24,6 +27,7 @@ class App extends React.Component {
     decription2: undefined,
     lat2: undefined,
     lon2: undefined,
+    forecast: [],
     error2: undefined
   }
 
@@ -35,15 +39,19 @@ class App extends React.Component {
     var city2 = e.target.elements.city2.value;
     var country2 = e.target.elements.country2.value;
 
-    var api_call1 = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city1},${country1}&appid=${API_KEY}&units=metric`);
-    var data1 = await api_call1.json();
+    var api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city1},${country1}&appid=${API_KEY}&units=metric`);
+    var data1 = await api_call.json();
 
-    var api_call2 = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city2},${country2}&appid=${API_KEY}&units=metric`);
-    var data2 = await api_call2.json();
+    api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city2},${country2}&appid=${API_KEY}&units=metric`);
+    var data2 = await api_call.json();
+
+    if (data2.coord) {
+      api_call = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data2.coord.lat}&lon=${data2.coord.lon}&
+exclude=current,minutely,hourly&appid=${API_KEY}&units=metric`);
+      data3 = await api_call.json();
+    }
 
     if (city1 && country1 && data1.cod === 200) {
-      console.log(data1);
-
       this.setState({
         temperature1: data1.main.temp,
         city1: data1.name,
@@ -57,7 +65,7 @@ class App extends React.Component {
     } else {
       this.setState({
         temperature1: undefined,
-        city1: undefined,
+        city1: "Error",
         country1: undefined,
         humidity1: undefined,
         description1: undefined,
@@ -68,7 +76,7 @@ class App extends React.Component {
     }
 
     if (city2 && country2 && data2.cod === 200) {
-      console.log(data2);
+      // console.log(data2);
 
       this.setState({
         temperature2: data2.main.temp,
@@ -78,23 +86,27 @@ class App extends React.Component {
         description2: data2.weather[0].description,
         lat2: data2.coord.lat,
         lon2: data2.coord.lon,
+        forecast: data3.daily,
         error2: ""
       });
+      console.log(this.state.forecast);
     } else {
       this.setState({
         temperature2: undefined,
-        city2: undefined,
+        city2: "Error",
         country2: undefined,
         humidity2: undefined,
         description2: undefined,
         lat2: undefined,
         lon2: undefined,
+        forecast: undefined,
         error2: "Please enter valid second city and country."
       });
     }
     console.log(this.state);
-
   }
+
+
 
   render() {
     return (
@@ -121,13 +133,31 @@ class App extends React.Component {
                     </div>
                     <div label={this.state.city2}>
                       <Weather
-                        temperature={this.state.temperature2}
+                        // temperature={this.state.temperature2}
+                        temperature={data3.daily[2].temp.day}
                         humidity={this.state.humidity2}
                         city={this.state.city2}
                         country={this.state.country2}
                         description={this.state.description2}
                         error={this.state.error2}
                       />
+                    </div>
+                    <div label={"Forecast"}>
+                      <Tabs>
+                        {days.map(day => (
+                          <div label={`Day ${day}`}>
+                            <Weather
+                              temperature={data3.daily[day].temp.day}
+                              humidity={data3.daily[day].humidity}
+                              city={this.state.city2}
+                              country={this.state.country2}
+                              description={data3.daily[day].weather[0].description}
+                              error={this.state.error2}
+                            />
+                          </div>
+                        ))}
+                      </Tabs>
+
                     </div>
                   </Tabs>
                   }
